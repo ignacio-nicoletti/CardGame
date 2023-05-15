8278c4dd1cf4d2874df6a5f2b023b844c925ec16
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-05-15 17:33:27 -0300

    card modify

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -7,7 +7,7 @@ import copa from '../../assets/valores/copas.png';
 import espada from '../../assets/valores/espada.png';
 import oro from '../../assets/valores/oro.png';
 
-const Cards = ({jugador1, setJugador1 ,valor, palo}) => {
+const Cards = ({jugador1, setJugador1 ,valor, palo,typeRound}) => {
 
 let imgPalo={
 "oro":oro,
@@ -21,9 +21,10 @@ let imgPalo={
   let filterCard;
 
   const handlerclick = () => {
+    if(typeRound==="ronda"){
     filterCard = jugador1.cardPersona.filter (e => e.valor !== valor || e.palo !==palo);
     setJugador1({...jugador1,cardApostada:[{valor,palo}],cardPersona:filterCard})
-
+    }
   };
 
   return (
--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -30,7 +30,7 @@ export default function Game () {
     cardPersona: [],
     apuestaP: 0,
     cardsganadas: '',
-    cardApostada: [{valor: '', palo: ''}],
+    cardApostada: [{valor: 4, palo: ''}],
     myturn: false, //boolean
     cumplio: '', //boolean
     obligado: false, //boolean
@@ -40,7 +40,7 @@ export default function Game () {
     cardPersona: [],
     apuestaP: 0,
     cardsganadas: '',
-    cardApostada: [{valor: '', palo: ''}],
+    cardApostada: [{valor: 3, palo: ''}],
     myturn: false, //boolean
     cumplio: '', //boolean
     obligado: false, //boolean
@@ -50,7 +50,7 @@ export default function Game () {
     cardPersona: [],
     apuestaP: 0,
     cardsganadas: '',
-    cardApostada: [{valor: '', palo: ''}],
+    cardApostada: [{valor: 2, palo: ''}],
     myturn: false, //boolean
     cumplio: '', //boolean
     obligado: false, //boolean
@@ -67,13 +67,13 @@ export default function Game () {
     vuelta: 1,
     numeroRonda: 1,
     cardPorRonda: 1,
-    typeRound: 'apuesta', //apuesta o ronda
+    typeRound: 'ronda', //apuesta o ronda
     turnoJugador: 1, //1j 2j 3j 4j
     ApuestaTotal: jugador1.apuestaP +
       jugador2.apuestaP +
       jugador3.apuestaP +
       jugador4.apuestaP, //suma de la apuesta de todos
-    CardGanadoraxRonda: [],
+    CardGanadoraxRonda: [{valor: '', palo: ''}],
     points: 5, //puntos que suma
   });
 
@@ -94,77 +94,83 @@ export default function Game () {
     setJugador3 ({...jugador3, cardPersona: jugadores.jugador3});
     setJugador4 ({...jugador4, cardPersona: jugadores.jugador4});
   };
+const comprobar=()=>{
 
   if (
-    (jugador1.cardApostada.valor < jugador2.cardApostada.valor) &&
-    (jugador1.cardApostada.valor <jugador3.cardApostada.valor)  && 
-    (jugador1.cardApostada.valor <jugador4.cardApostada.valor) 
-    
-  ){setRonda({...ronda,CardGanadoraxRonda:jugador1.cardApostada[0].valor})}
+    jugador1.cardApostada[0].valor > jugador2.cardApostada[0].valor &&
+    jugador1.cardApostada[0].valor > jugador3.cardApostada[0].valor &&
+    jugador1.cardApostada[0].valor > jugador4.cardApostada[0].valor
+  ) {
+    console.log( " es la mas grande");
+    setJugador1({...jugador1,cardsganadas:1})
+     setRonda ({...ronda, CardGanadoraxRonda: jugador1.cardApostada});
+  }
+}
 
-  console.log(ronda.CardGanadoraxRonda);
-    return (
-      <div className={style.contain}>
-        <div className={style.containCardPropias}>
-          <div className={style.CardPropias}>
+  return (
+    <div className={style.contain}>
+      <div className={style.containCardPropias}>
+        <div className={style.CardPropias}>
 
-            {jugador1.cardPersona.map (card => (
-              <Cards
-                valor={card.valor}
-                palo={card.palo}
-                jugador1={jugador1}
-                setJugador1={setJugador1}
-              />
-            ))}
-          </div>
-        </div>
-
-        {cantUser === 4
-          ? <div className={style.jugadorestres}>
-              <div className={style.jugador2}>
-                <Jugadores jugador={jugador2} />W
-              </div>
-              <div className={style.jugador3}>
-                <Jugadores jugador={jugador3} />
-              </div>
-              <div className={style.jugador4}>
-                <Jugadores jugador={jugador4} />
-              </div>
-            </div>
-          : <div className={style.jugadoresdos}>
-              <div>
-                <Jugadores jugador={jugador2} />
-              </div>
-              <div>
-                <Jugadores jugador={jugador3} />
-              </div>
-            </div>}
-        <div />
-        <div className={style.infoPartida}>
-          <p>Cartas Repartidas: {ronda.cardPorRonda} </p>
-          <p>Apuesta General: {ronda.ApuestaTotal}</p>
-          <p>Carta Ganadora: {ronda.CardGanadoraxRonda.valor}</p>
-          <p>Vuelta: {ronda.vuelta}</p>
-          <p>Ronda: {ronda.numeroRonda}</p>
-        </div>
-        <div className={style.infoPropia}>
-          <p>Apuesta propia: {jugador1.apuestaP} </p>
-
-          <p>Cumplio: {jugador1.cumplio === true ? '✔' : '❌'}</p>
-        </div>
-        <button onClick={repartir}>repartir</button>
-        <button onClick={gameInit}>Comenzar Juego</button>
-
-        {ronda.typeRound === 'apuesta' && jugador1.myturn === true
-          ? <Apuesta jugador1={jugador1} setJugador1={setJugador1} />
-          : ''}
-        <div className={style.CardApostada}>
-
-          {jugador1.cardApostada[0].valor &&
-            jugador1.cardApostada.map (card => (
-              <Cards valor={card.valor} palo={card.palo} />
-            ))}
+          {jugador1.cardPersona.map (card => (
+            <Cards
+              valor={card.valor}
+              palo={card.palo}
+              jugador1={jugador1}
+              setJugador1={setJugador1}
+              typeRound={ronda.typeRound}
+            />
+          ))}
         </div>
       </div>
-    );
+
+      {cantUser === 4
+        ? <div className={style.jugadorestres}>
+            <div className={style.jugador2}>
+              <Jugadores jugador={jugador2} />W
+            </div>
+            <div className={style.jugador3}>
+              <Jugadores jugador={jugador3} />
+            </div>
+            <div className={style.jugador4}>
+              <Jugadores jugador={jugador4} />
+            </div>
+          </div>
+        : <div className={style.jugadoresdos}>
+            <div>
+              <Jugadores jugador={jugador2} />
+            </div>
+            <div>
+              <Jugadores jugador={jugador3} />
+            </div>
+          </div>}
+      <div />
+      <div className={style.infoPartida}>
+        <p>Cartas Repartidas: {ronda.cardPorRonda} </p>
+        <p>Apuesta General: {ronda.ApuestaTotal}</p>
+        <p>Carta Ganadora: {ronda.CardGanadoraxRonda[0].valor} {ronda.CardGanadoraxRonda[0].palo}</p>
+        <p>Vuelta: {ronda.vuelta}</p>
+        <p>Ronda: {ronda.numeroRonda}</p>
+      </div>
+      <div className={style.infoPropia}>
+        <p>Apuesta propia: {jugador1.apuestaP} </p>
+        <p>Ganadas: {jugador1.cardsganadas}</p>
+        <p>Cumplio: {jugador1.cumplio === true ? '✔' : '❌'}</p>
+      </div>
+      <button onClick={repartir}>repartir</button>
+      <button onClick={gameInit}>Comenzar Juego</button>
+      <button onClick={comprobar}>Comprobar</button>
+
+      {ronda.typeRound === 'apuesta' && jugador1.myturn === true
+        ? <Apuesta jugador1={jugador1} setJugador1={setJugador1} />
+        : ''}
+      <div className={style.CardApostada}>
+
+        {jugador1.cardApostada[0].valor &&
+          jugador1.cardApostada.map (card => (
+            <Cards valor={card.valor} palo={card.palo} />
+          ))}
+      </div>
+    </div>
+  );
 }
